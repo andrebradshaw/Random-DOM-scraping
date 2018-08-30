72c53b8f073a613c8bdb5156a7c34a42b922e33d
Andre Bradshaw <bradshaw.andre@gmail.com>
2018-08-29 23:36:29 -0400

    Update ShowKeyStrokes.js

--- ShowKeyStrokes.js
+++ ShowKeyStrokes.js
@@ -2,46 +2,51 @@
 
 
 function createContainer(len){
-var popwin = document.createElement("div");
-document.body.appendChild(popwin);
-popwin.setAttribute("id", "popwindow");
-popwin.style.display = "inline-block";
-popwin.style.position = "fixed";
-popwin.style.top = "25%";
-popwin.style.left = "65%";
-popwin.style.width = "5%";
-popwin.style.height = "5%";
-popwin.style.background = "darkGrey";
-popwin.style.borderRadius = "1em";
-popwin.style.zIndex ="10000";
-popwin.style.fonFamily = '"Courier New", monospace';
+	var popwin = document.createElement("div");
+	document.body.appendChild(popwin);
+	popwin.setAttribute("id", "popwindow");
+	popwin.style.display = "inline-block";
+	popwin.style.position = "fixed";
+	popwin.style.top = "25%";
+	popwin.style.left = "65%";
+	popwin.style.width = "5%";
+	popwin.style.height = "5%";
+	popwin.style.background = "darkGrey";
+	popwin.style.borderRadius = "1em";
+	popwin.style.padding = "6px";
+	popwin.style.textAlign = "center";
+	popwin.style.zIndex ="10000";
+	popwin.style.fonFamily = '"Courier New", monospace';
+
 }
 
 function killContainer(){
-	setTimeout(()=>{
 		document.body.removeChild(document.getElementById("popwindow"));
-	},1666);
 }
 
 function addKeyStroke(txt){
 	setTimeout(()=>{
 		document.getElementById("popwindow").innerText = document.getElementById("popwindow").innerText+txt;
 
-    },333);
+    },63);
 }
 
-window.addEventListener('keydown', function(event) {
 
-	if(document.getElementById("popwindow") == null){
+window.addEventListener('keydown', function(event) {
+var win = document.getElementById("popwindow");
+
+	if(win == null){
     	createContainer();
 		var k = event.key.toString();
 		addKeyStroke(k);
-		document.getElementById("popwindow").style.width = (5+(document.getElementById("popwindow").innerText.length.length*.2))+"%";
-		killContainer();
+
 	}else{
+		var txtLen = win.innerText.length;
 		var k = event.key.toString();
 		addKeyStroke(k);
-		document.getElementById("popwindow").style.width = (5+(document.getElementById("popwindow").innerText.length*.2))+"%";
+		document.getElementById("popwindow").style.width = (5+(txtLen*.8))+"%";
+		if(txtLen > 15){
 		killContainer();
+		}
 	}
 });
